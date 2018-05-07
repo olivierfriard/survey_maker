ef5d6e4713e50f9c17e337c760380a6852e5c44a
Olivier Friard <olivier.friard@unito.it>
2018-05-07 17:18:39 +0200

    add licence

--- survey.py
+++ survey.py
@@ -1,7 +1,27 @@
 #!/usr/bin/env python3
 """
+
+
+
 Survey maker
-2018 (c) Olivier Friard
+Copyright 2018 Olivier Friard
+
+This file is part of "Survey maker".
+
+  "Survey maker" is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 3 of the License, or
+  any later version.
+
+  "Survey maker" is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not see <http://www.gnu.org/licenses/>.
+
+
 """
 
 import os
